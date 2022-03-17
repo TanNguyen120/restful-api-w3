9ce6036277747a58b78c496e9240944f47c52b9a
Tan Nguyen <tannguyen1220@gmail.com>
2022-03-17 08:27:21 +0700

    ADD cors to all route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -27,6 +28,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors({
+  origin: 'https://localhost:5500',
+  method: 'GET, PUT, POST, DELETE '
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
