b1269373df5be9fa3be0439af0e4033388c5738e
Tan Nguyen <tannguyen1220@gmail.com>
2022-04-03 19:35:29 +0700

    fix bug

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../model/knex");
-const bcrypt = require('bcrypt');
-const auth = require('../middleware/auth');
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
     try {
@@ -11,14 +9,14 @@ router.get('/', async function (req, res, next) {
             const opt = {
                 ignoreExpration: true,
             }
-            await jwt.verify(token, 'mySecretKey', opt, (err, decoded) => {
+            await jwt.verify(token, 'mySecretKey', opt, async (err, decoded) => {
                 if (err) {
                     res.status(401).json({ message: "token is not valid" });
                 } else {
                     const userId = decoded.userId;
                     const userName = decoded.username;
-                    const refreshToken = decoded.refreshToken;
-                    const savedReFreshToken = await knex.knexObj("user").where("id", userId).select("username");
+                    const refreshToken = req.body.refreshToken;
+                    const savedReFreshToken = await knex.knexObj("user").where("id", userId).select("freshToken");
                     if (refreshToken === savedReFreshToken[0]) {
                         const accessToken = jwt.sign({ username: userName, userId: userId }, 'mySecretKey', { expiresIn: '1h' });
                         res.status(200).json({ message: "refreshed", accessToken: accessToken, refreshToken: refreshToken });
